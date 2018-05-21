401cb941e7b504aaf13dbd20f81940e0c232c9de
Patrick Zinner <zinnsoldat91@gmail.com>
2018-05-21 12:32:47 +0200

    backend init

---
+++ basicauthbackend/src/main/java/com/patrickzinner/demo/basicauthbackend/BasicauthbackendApplication.java
@@ -0,0 +1,12 @@
+package com.patrickzinner.demo.basicauthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicauthbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicauthbackendApplication.class, args);
+	}
+}
---
+++ basicauthbackend/src/test/java/com/patrickzinner/demo/basicauthbackend/BasicauthbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.patrickzinner.demo.basicauthbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicauthbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
